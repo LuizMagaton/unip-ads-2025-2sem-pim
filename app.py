6c20275200d6737433b1ebbe7b3185be53aefdd9
LuizMagaton <luizmagaton22@gmail.com>
2025-08-26 22:25:09 -0300

    telas novas

--- app.py
+++ app.py
@@ -6,8 +6,16 @@ import bcrypt
 app = Flask(__name__)
 
 @app.route("/")
-def menu():
-    return render_template("menu.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/esquecisenha")
+def esquecisenha():
+    return render_template("esqueci_senha.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
